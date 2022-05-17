5f72483824e2bfe4fd74361fef877c5d05bd615f
Seven <chase@chasehuegel.io>
2022-05-17 02:33:33 -0400

    initial commit
    - Updated to net5.0
    - Updated to latest OpenTK
    - Updated to latest ImGui.NET

---
+++ Swordfish.Library/Types/Optional.cs
@@ -0,0 +1,15 @@
+﻿namespace Swordfish
+{
+    //  Represents an optional field
+    public struct Optional<T>
+    {
+        public bool Enabled;
+        public T Value;
+
+        public Optional(T value)
+        {
+            Enabled = true;
+            Value = value;
+        }
+    }
+}
